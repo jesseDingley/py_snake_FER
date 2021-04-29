c00b855e4c2faabde29ba8b397f38c80860bded2
Etienne Coulmeau <coulmeauet@eisti.eu>
2021-04-29 10:49:23 +0300

    blibli

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
 # py_snake_facial_expressions
 Classic snake game developed in Python. Movement of snake controlled by facial expressions. 
 
+## Execution
+To currently execute the program and play the game, you must have the following packages :
+- pygame
+- os
+- cv2
+- numpy
+- threading
+- sys
+- random
+- shelve
+- keras
+- tensorflow
+
+After verifying that you have all the required packages, please use the command :
+```bash
+python3 main.py
+```
+
+You can now play the game.
+
+## How to play
+### Change from "face" mode to "classic" mode and vice-versa
+While not playing, press "M : Classic" or "M : Face" to change from one mode to the other
+### Mode "classic"
+You can press "play" and use the keyboard's arrows to move the snake.
+### Mode "face"
+Still in development (allows you to play snake with emotion recognition).
+### Quit the game
+Press "quit"
+
+
 ## Files
 ### Detection of facial emotions: 
 ```bash
--- main.py
+++ main.py
@@ -450,6 +450,51 @@ def init_best_score():
     d.close()
     return best_score
             
+def emotion_detection(thread_running):
+    global current_mode
+    cap=cv2.VideoCapture(0)
+    i = 0
+    while thread_running.is_set():
+        # i += 1
+        # if i % 200 == 0:
+        #     print("current_mode:", current_mode)
+        if current_mode == "classic":
+            pass
+        else:
+            ret,test_img=cap.read()# captures frame and returns boolean value and captured image
+            if not ret:
+                continue
+            gray_img= cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+
+            faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+
+            for (x,y,w,h) in faces_detected:
+                cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0),thickness=7)
+                roi_gray=gray_img[y:y+w,x:x+h]#cropping region of interest i.e. face area from  image
+                roi_gray=cv2.resize(roi_gray,(48,48))
+                img_pixels = image.img_to_array(roi_gray)
+                img_pixels = np.expand_dims(img_pixels, axis = 0)
+                img_pixels /= 255
+
+                predictions = model.predict(img_pixels)
+
+                #find max indexed array
+                max_index = np.argmax(predictions[0])
+
+                emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+                predicted_emotion = emotions[max_index]
+
+                cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+
+            resized_img = cv2.resize(test_img, (500, 350))
+            cv2.imshow('Facial emotion analysis ',resized_img)
+
+            if cv2.waitKey(10) == ord('q'):#wait until 'q' key is pressed
+                break
+
+    cap.release()
+    cv2.destroyAllWindows
+
 
 
 """
@@ -467,9 +512,17 @@ apple = Apple(generate_apple_coords())
 # init best_score
 best_score = init_best_score()
 
+# load model
+model = model_from_json(open("fer.json", "r").read())
+# load weights
+model.load_weights('fer.h5')
+face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 # thread to run the emotion recognizer
 thread_running = threading.Event()
 thread_running.set()
+th = threading.Thread(target = emotion_detection, args = (thread_running, ))
+th.start()
 
 
 
@@ -496,6 +549,7 @@ while True:
 
             # if quit button is clicked: exit game
             if mouse_hover_over_btn("quit", mouse_x, mouse_y):
+                thread_running.clear()
                 pygame.quit()
                 sys.exit()
 
@@ -517,12 +571,13 @@ while True:
                 # increment m_count
                 m_count += 1
 
-        # key pressed
-        if event.type == pygame.KEYDOWN and snake_can_move:
-            # want to change directio so add 1 (if it passes 1 then change_direction is disabled)
-            nb_desired_changes += 1
-            if nb_desired_changes == 1:
-                snake.change_direction_with_keys(event)
+        if current_mode == "classic":
+            # key pressed
+            if event.type == pygame.KEYDOWN and snake_can_move:
+                # want to change directio so add 1 (if it passes 1 then change_direction is disabled)
+                nb_desired_changes += 1
+                if nb_desired_changes == 1:
+                    snake.change_direction_with_keys(event)
             
 
   
